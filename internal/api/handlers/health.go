655ef35670aea8b01fb484633c8d07a77548e08e
Nico Hämäläinen <nibsivaan@gmail.com>
2024-07-19 08:05:49 +0300

    added swagger annotations
    Signed-off-by: Nico Hämäläinen <nibsivaan@gmail.com>

--- internal/api/handlers/health.go
+++ internal/api/handlers/health.go
@@ -4,6 +4,14 @@ import (
 	"github.com/gofiber/fiber/v3"
 )
 
+// Health godoc
+// @Summary Health
+// @Description Get health status
+// @Tags health
+// @Accept json
+// @Produce json
+// @Success 200 {object} map[string]string
+// @Router /health [get]
 func Health(c fiber.Ctx) error {
 	return c.JSON(fiber.Map{
 		"status": "ok",
